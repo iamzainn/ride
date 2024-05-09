b9df1df8d34c0ba3fbf64afd18212ba7b3583fc6
iamzainn <iamzainnf@gmail.com>
2024-05-09 23:41:47 +0500

    Add Suspense payment

---
+++ src/app/payment/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaymenLoadingPage = () => {
+  return (
+    <div className='payment_loading_page'>
+      <h1>Loading</h1>
+    </div>
+  )
+}
+
+export default PaymenLoadingPage
